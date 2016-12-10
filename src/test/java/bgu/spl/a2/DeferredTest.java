48a682148d35cd32e20d78dc77e368bd42fbaf69
rotemba <rotemba@post.bgu.ac.il>
2016-12-10 23:59:44 +0200

    change

---
+++ src/test/java/bgu/spl/a2/DeferredTest.java
@@ -0,0 +1,43 @@
+package bgu.spl.a2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 10/12/2016.
+ */
+public class DeferredTest {
+    private Deferred<T> check();
+    /*
+        Create an instance of the class.
+        The class must have a public default constructor.
+        For each method that fits: @Test public void _____ () { … }
+        Run a method with the tag @Before (e.g., setUp()) - preparing the conditions for the test.
+        Run the method _____ ()
+        Display test results
+        Run a method with the tag @After (e.g., tearDown()) - returning the system to normal functionality.
+
+     */
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void isResolved() throws Exception {
+
+    }
+
+    @Test
+    public void resolve() throws Exception {
+
+    }
+
+    @Test
+    public void whenResolved() throws Exception {
+
+    }
+
+}
