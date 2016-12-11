78bde244f3aed45d9f74e6b5673d0c2180ca6c54
Bredesh <edenkeshet@gmail.com>
2016-12-11 23:33:29 +0200

    change in VersionMonitorTest

--- src/test/java/bgu/spl/a2/VersionMonitorTest.java
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -8,21 +8,37 @@ import static org.junit.Assert.*;
  * Created by user on 10/12/2016.
  */
 public class VersionMonitorTest {
+	VersionMonitor verMon = new VersionMonitor(0);
+	@Test
+	public void getVersion() throws Exception {
+		int i = verMon.getVersion();
+		assertEquals(1,i);
 	
-	
-    @Test
-    public void getVersion() throws Exception {
+		verMon.inc();;
+		i = verMon.getVersion();
+		assertEquals(1,i);
+		
+		
+		
+	}
 
-    }
+	@Test
+	public void inc() throws Exception {
+//		VersionMonitor verMon = new VersionMonitor(0);
+		int i = verMon.getVersion();
+		verMon.inc();
+		verMon.inc();
+		verMon.inc();
+		verMon.inc();
+		int inc = verMon.getVersion();
+		assertEquals(i+4,inc);
+	}
 
-    @Test
-    public void inc() throws Exception {
-
-    }
-
-    @Test
-    public void await() throws Exception {
-
-    }
+	@Test
+	public void await() throws Exception {
+		System.out.println(verMon.getVersion());
+		verMon.await(0);
+		verMon.await(1);
+	}
 
 }
